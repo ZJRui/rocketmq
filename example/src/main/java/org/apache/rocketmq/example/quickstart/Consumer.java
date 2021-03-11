39d86e6367fca597d21ec816118101c041335759
sachin <40294352@qq.com>
2021-03-12 00:51:53 +0800

    1,拷贝配置文件 2，拷贝rocketmqspring-boot 项目

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -57,7 +57,8 @@ public class Consumer {
         /*
          * Subscribe one more more topics to consume.
          */
-        consumer.subscribe("TopicTest", "*");
+      //  consumer.subscribe("TopicTest", "*");
+        consumer.subscribe("TopicTest1234", "*");//订阅事务消息案例中的主题
 
         /*
          *  Register callback to execute on arrival of messages fetched from brokers.
