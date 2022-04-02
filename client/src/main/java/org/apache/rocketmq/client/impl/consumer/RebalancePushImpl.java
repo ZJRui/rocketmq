4a179a55114cfd718919ed11717f83e79bb34bb6
zjr <40123628+ZJRui@users.noreply.github.com>
2022-04-02 12:57:48 +0800

    讨论：消费者位移提交只提交到内存，没有实时提交到Broker，这对消息重复消费会有什么影响的问题

--- client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalancePushImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalancePushImpl.java
@@ -83,12 +83,33 @@ public class RebalancePushImpl extends RebalanceImpl {
 
     @Override
     public boolean removeUnnecessaryMessageQueue(MessageQueue mq, ProcessQueue pq) {
+
         /**
-         * 对MessageQueue进行持久化
+         *ConsumeRequest中 消息消费成功后会提交位移，
+         * org.apache.rocketmq.client.consumer.store.RemoteBrokerOffsetStore#updateOffset(org.apache.rocketmq.common.message.MessageQueue, long, boolean)
+         *
+         * 需要注意的是 位移提交仅仅是保存在内存中，没有实时提交给Broker。当consumer关闭 或者RebalanceImpl重分配移除当前MessageQueue的时候才会将位移同步
+         * 给Broker（updateConsumeOffsetToBroker），也就是说不是每消费一个消息就提交位移给broker，另外会有一个定时任务定期执行 persistAllConsumerOffset将位移同步给Broker
+         *
+         *
+         * 我是不是可以这么认为， 由于Consumer消费之后 位移提交到本地内存，没有及时同步给Broker， 如果其他进程Consumer获取到了MessageQueue 然后从中拉取消息进行消费，这就会导致重复消费的问题。这个观点需要分情况讨论
+         *
+         * （1）如果当先消费者是顺序消费 ，对于当前消费者而言，重分配之后 对于那些需要不再属于当前Consumer的MessageQueue会执行RebalancePushImpl#removeUnnecessaryMessageQueue()，在remove方法中会先
+         * 将本地位移同步给Broker updateConsumeOffsetToBroker ，然后再发送请求给Broker释放 MessageQueue上的锁。  那么也就是说其他进程的consumer 获得了这个MessageQueue ，想要从这个MessageQueue中拉取消息需要首先获取到这个MessageQueue的锁然后从
+         * MessageQueue中拉取消息的时候 一定是在 当前consumer提交位移之后。 只有是顺序消费的consumer 才会 在拉取messageQueuez中的消息前发送给Broker消息对MessageQueue加锁，移除MessageQueue的时候发送给Broker消息释放锁。
+         * 在这种情况下 没有实时同步给Broker是不会引起什么问题的，因为即便其他进程Consumer获得了MessageQueue，但是他无法在Broker上获取该MessageQueue的锁，也就无法消费MessageQueue中的消息。 必须要等到当前Consumer 主动释放MessageQueue的锁，而当前Consumer
+         * 在释放messageQueue锁的时候 必然会先提交位移给Broker。
+         *
+         * （2）对于非顺序消费的消费者， 一旦其他进程Consumer获得了 MessageQueue，那么就可以从这个MessageQueue中立刻拉取消息，这个时候可能原来的consumer 还没有 移除这个MessageQueue（移除MessageQueue的时候会提交位移给Broker）且还没有到达
+         * 提交位移给Broker的时钟周期（消费端有定时任务定时提交位移给broker），那么这种情况下就会存在重复消费。
+         *
          *
          */
         this.defaultMQPushConsumerImpl.getOffsetStore().persist(mq);
         this.defaultMQPushConsumerImpl.getOffsetStore().removeOffset(mq);
+        /**
+         * 如果是顺序消费，则发送给broker请求释放messageQueue 上的锁
+         */
         if (this.defaultMQPushConsumerImpl.isConsumeOrderly()
             && MessageModel.CLUSTERING.equals(this.defaultMQPushConsumerImpl.messageModel())) {
             try {
